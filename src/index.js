d00cabcbb0f816ee6976a6a691d7efbc8a7ae3ea
Ryan Gleason <ryangleason82@gmail.com>
2019-05-02 06:55:25 -0400

    JSX

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import ReactDOM from "react-dom";
 
 // Create a react function based component
 const App = () => {
+	const buttonText = { text: "Click me!" };
+	const style = { backgroundColor: "blue", color: "white" };
+	const enterName = "Enter name: ";
+
 	// JSX
-	return <div>Hi There</div>;
+	return (
+		<div>
+			<label className="label" htmlFor="name">
+				{enterName}
+			</label>
+			<input id="name" type="text" />
+			<button style={style}>{buttonText.text}</button>
+		</div>
+	);
 };
 
 // Take the react component and show it on the screen
