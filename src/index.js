1f2dce84bf74f68aac25bdc00692386c6dcd4f2c
Ryan Gleason <ryangleason82@gmail.com>
2019-05-02 06:12:00 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react function based component
+const App = () => {
+	// JSX
+	return <div>Hi There</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
