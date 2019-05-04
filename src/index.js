735b45fe8199b60a74a38ffa55acb5598a715405
Ryan Gleason <ryangleason82@gmail.com>
2019-05-04 10:01:25 -0400

    Nested components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // Create a react function based component
 const App = () => {
-	const buttonText = "Click Me";
-	const style = { backgroundColor: "blue", color: "white" };
-	const enterName = "Enter name: ";
-
-	// JSX
 	return (
-		<div>
-			<label className="label" htmlFor="name">
-				{enterName}
-			</label>
-			<input id="name" type="text" />
-			<button style={style}>{buttonText}</button>
+		<div className="ui container comments">
+			<ApprovalCard>
+				{" "}
+				<div>
+					<h4>Warning</h4>Are you sure you want to do this?
+				</div>{" "}
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author={faker.name.firstName()}
+					timeAgo={"Today at 4:04"}
+					text={faker.random.words()}
+					avatar={faker.image.avatar()}
+				/>{" "}
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author={faker.name.firstName()}
+					timeAgo="Today at 5:34"
+					text={faker.random.words()}
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author={faker.name.firstName()}
+					timeAgo="Today at 10:23"
+					text={faker.random.words()}
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
